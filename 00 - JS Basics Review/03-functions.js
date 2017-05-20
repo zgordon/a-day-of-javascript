f5018822afa3b09c12ab3e0495523f0fa2e41f8d
Zac Gordon <education@zacgordon.com>
2017-05-20 18:27:51 -0400

    Beautifying

--- 00 - JS Basics Review/03-functions.js
+++ 00 - JS Basics Review/03-functions.js
@@ -8,15 +8,15 @@ getPosts();
 
 // Function declaration with parameter
 function getUser( userId ) {
-	let user = 'User ' + 1; // Will learn how to get with API
-	return user;
+  let user = 'User ' + 1; // Will learn how to get with API
+  return user;
 }
 getUsers( 1 );
 
 
 // Function assigned to variable
 let post = function getPost( postId ) {
-	let post = 'Post ' + userId; // Will learn how to get with API
-	return post;
+  let post = 'Post ' + userId; // Will learn how to get with API
+  return post;
 };
 post( 1 );
