0ff01dfd7f4228e2a37a82efc1c4986296c3f2a0
Zac Gordon <education@zacgordon.com>
2017-05-20 18:28:52 -0400

    Beautifying

--- 01 - The DOM/01.00 - Demos/01-methods.js
+++ 01 - The DOM/01.00 - Demos/01-methods.js
@@ -1,8 +1,8 @@
 let h1 = document.getElementById('main-title'),
-		links = document.getElementsByTagName('a'),
-		pTags = document.getElementsByTagName('p'),
-		content = document.getElementsByClassName('content'),
-		input = document.querySelector('input.username'),
-		listItems = document.querySelectorAll('li');
+  links = document.getElementsByTagName('a'),
+  pTags = document.getElementsByTagName('p'),
+  content = document.getElementsByClassName('content'),
+  input = document.querySelector('input.username'),
+  listItems = document.querySelectorAll('li');
 
 console.log( h1, links, pTags, content, input, listItems);
